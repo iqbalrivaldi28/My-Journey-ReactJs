a753f7946663e8c17dc5bb7fdcbdf653270f4e41
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-23 17:21:08 +0700

    feat: nested rendering card product

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -5,13 +5,13 @@ const Products = () => {
   return (
     <div className="flex justify-center py-5">
       <CardProduct>
-        <CardProduct.Header image="/public/images/shoes-1.jpg" />
+        <CardProduct.Header image="/public/images/sepatu.jpg" />
         <CardProduct.Body title="Sepatu Pertama">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore beatae consectetur earum sed veniam neque repellat amet labore error omnis?</CardProduct.Body>
         <CardProduct.Footer price="Rp. 1.000.000" />
       </CardProduct>
 
       <CardProduct>
-        <CardProduct.Header image="/public/images/shoes-1.jpg" />
+        <CardProduct.Header image="/public/images/sepatu.jpg" />
         <CardProduct.Body title="Sepatu Kedua">Lorem ipsum, dolor sit amet consectetur adipisicing. </CardProduct.Body>
         <CardProduct.Footer price="Rp. 500.000" />
       </CardProduct>
